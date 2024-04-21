a16f1d226b7d1f91ef6128719a327a1dd15a3a47
Álvaro Marques <alvaromarques18@gmail.com>
2024-04-21 15:04:10 -0300

    refactor: turning primary button into a component

---
+++ src/components/Login/PrimaryButton.tsx
@@ -0,0 +1,53 @@
+import {GestureResponderEvent, Pressable, StyleSheet, Text} from "react-native";
+import { Colors } from "../../assets/constants/colors";
+
+export function PrimaryButton({
+  onPress,
+  text,
+  isDisable,
+}: {
+  onPress: (event: GestureResponderEvent) => void;
+  text: string;
+  isDisable?:boolean
+}) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({pressed}) =>
+        pressed ? [styles.btn, styles.pressed] : styles.btn
+      }
+      disabled={isDisable}
+      >
+      <Text style={styles.btnText}>{text}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  pressed: {
+    backgroundColor: Colors.secundaryColorDark,
+  },
+
+  btn: {
+    backgroundColor: Colors.secundaryColor,
+    height: 48,
+    width: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 24,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4,
+    elevation: 7,
+  },
+
+  btnText: {
+    fontSize: 20,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
